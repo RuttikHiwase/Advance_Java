03be0b6f0a7c438f3c9ed4fc04092ae17d25ccad
RuttikHiwase <89509037+RuttikHiwase@users.noreply.github.com>
2025-02-06 15:08:12 +0530

    Add files via upload

---
+++ MyPracticeOne/src/test/java/com/codingshuttle/PracticeOne/MyPracticeOne/TutorialOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.PracticeOne.MyPracticeOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
